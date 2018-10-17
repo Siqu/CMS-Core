a8a660ab84cf2f784fd94de0a4f9efa62c768d37
Sebastian Paulmichl <sebastian.paulmichl@tiscover.com>
2018-10-17 13:16:57 +0200

    [TASK] enabled bundles, removed .idea

---
+++ src/API/CMSAPIBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Siqu\CMS\API;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class CMSAPIBundle
+ * @package Siqu\CMS\API
+ */
+class CMSAPIBundle extends Bundle
+{
+
+}
