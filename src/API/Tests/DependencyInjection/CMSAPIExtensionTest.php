0c1f2781dc1b7f6e5413e72d3d55bd5cff9ef757
Sebastian Paulmichl <sebastian.paulmichl@tiscover.com>
2018-10-24 14:10:16 +0200

    [TASK] added gitlab ci configuration; added tests

---
+++ src/API/Tests/DependencyInjection/CMSAPIExtensionTest.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Siqu\CMS\API\Tests\DependencyInjection;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Siqu\CMS\API\DependencyInjection\CMSAPIExtension;
+use Siqu\CMS\API\EventListener\AcceptLanguageListener;
+use Siqu\CMS\API\EventListener\AcceptListener;
+use Siqu\CMS\API\Normalizer\EntityNormalizer;
+use Siqu\CMS\API\Security\ApiAuthenticator;
+use Siqu\CMS\API\Security\ApiUserProvider;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Class CMSAPIExtensionTest
+ * @package Siqu\CMS\API\Tests\DependencyInjection
+ */
+class CMSAPIExtensionTest extends TestCase
+{
+    /** @var ContainerBuilder|MockObject */
+    private $container;
+    /** @var CMSAPIExtension */
+    private $extension;
+
+    /**
+     * Should create instance.
+     */
+    public function testConstruct(): void
+    {
+        $this->assertInstanceOf(CMSAPIExtension::class, $this->extension);
+    }
+
+    /**
+     * Should load service definition.
+     */
+    public function testLoad(): void
+    {
+        $this->extension->load([
+            'siqu_cms_api' => []
+        ], $this->container);
+
+        $definition = $this->container->getDefinition('siqu.cms_api.event_listener.request.accept_language');
+        $this->assertEquals(AcceptLanguageListener::class, $definition->getClass());
+        $this->assertFalse($definition->isPublic());
+        $tags = $definition->getTags();
+        $this->assertArrayHasKey('kernel.event_listener', $tags);
+        $tag = $tags['kernel.event_listener'];
+        $this->assertEquals(KernelEvents::REQUEST, $tag[0]['event']);
+        $this->assertEquals(15, $tag[0]['priority']);
+
+        $definition = $this->container->getDefinition('siqu.cms_api.event_listener.request.accept');
+        $this->assertEquals(AcceptListener::class, $definition->getClass());
+        $this->assertFalse($definition->isPublic());
+        $tags = $definition->getTags();
+        $this->assertArrayHasKey('kernel.event_listener', $tags);
+        $tag = $tags['kernel.event_listener'];
+        $this->assertEquals(KernelEvents::REQUEST, $tag[0]['event']);
+        $this->assertEquals(17, $tag[0]['priority']);
+
+        $definition = $this->container->getDefinition('siqu.cms_api.normalizer.object.entity');
+        $this->assertEquals(EntityNormalizer::class, $definition->getClass());
+        $this->assertFalse($definition->isPublic());
+        $tags = $definition->getTags();
+        $this->assertArrayHasKey('serializer.normalizer', $tags);
+        $arguments = $definition->getArguments();
+        /** @var Reference $ref */
+        $ref = $arguments[0];
+        $this->assertEquals('doctrine.orm.entity_manager', $ref);
+        /** @var Reference $ref */
+        $ref = $arguments[1];
+        $this->assertEquals('serializer.mapping.class_metadata_factory', $ref);
+        /** @var Reference $ref */
+        $ref = $arguments[2];
+        $this->assertNull($ref);
+        /** @var Reference $ref */
+        $ref = $arguments[3];
+        $this->assertEquals('serializer.property_accessor', $ref);
+        /** @var Reference $ref */
+        $ref = $arguments[4];
+        $this->assertEquals('property_info', $ref);
+
+        $definition = $this->container->getDefinition('siqu.cms_api.security.provider.user');
+        $this->assertEquals(ApiUserProvider::class, $definition->getClass());
+        $this->assertFalse($definition->isPublic());
+        $arguments = $definition->getArguments();
+        /** @var Reference $ref */
+        $ref = $arguments[0];
+        $this->assertEquals('doctrine.orm.entity_manager', $ref);
+        /** @var Reference $ref */
+        $ref = $arguments[1];
+        $this->assertEquals('security.password_encoder', $ref);
+
+        $definition = $this->container->getDefinition('siqu.cms_api.security.authenticator');
+        $this->assertEquals(ApiAuthenticator::class, $definition->getClass());
+        $this->assertFalse($definition->isPublic());
+
+    }
+
+    /**
+     * Setup tests.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->container = new ContainerBuilder();
+
+        $this->extension = new CMSAPIExtension();
+    }
+}
---
+++ src/API/Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Siqu\CMS\API\Tests\DependencyInjection;
+
+use PHPUnit\Framework\TestCase;
+use Siqu\CMS\API\DependencyInjection\Configuration;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+/**
+ * Class ConfigurationTest
+ * @package Siqu\CMS\API\Tests\DependencyInjection
+ */
+class ConfigurationTest extends TestCase
+{
+    /** @var Configuration */
+    private $configuration;
+
+    /**
+     * Should create instance
+     */
+    public function testConstruct(): void
+    {
+        $this->assertInstanceOf(Configuration::class, $this->configuration);
+    }
+
+    /**
+     * Should return tree builder for bundle.
+     */
+    public function testGetConfigTreeBuilder(): void
+    {
+        $builder = $this->configuration->getConfigTreeBuilder();
+
+        $this->assertInstanceOf(TreeBuilder::class, $builder);
+
+        $tree = $builder->buildTree();
+
+        $this->assertEquals('siqu_cms_api', $tree->getName());
+    }
+
+    /**
+     * Setup tests.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->configuration = new Configuration();
+    }
+}
